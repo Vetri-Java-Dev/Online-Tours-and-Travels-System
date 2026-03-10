9af2bd12d0f31516fc488076f79def833086ac41
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-03-10 18:32:17 +0530

    Project structure pushed

---
+++ src/main/java/com/expleoProject/Tour/and/Travel/System/TourAndTravelSystemApplication.java
@@ -0,0 +1,13 @@
+package com.expleoProject.Tour.and.Travel.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAndTravelSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAndTravelSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expleoProject/Tour/and/Travel/System/TourAndTravelSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expleoProject.Tour.and.Travel.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAndTravelSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
