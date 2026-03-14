c7ef6b18009e66dadfe3a58b044977b03d02ce6c
Akksheetha <akkshee@gmail.com>
2026-03-14 13:36:37 +0530

    Created class and attributes

---
+++ src/main/java/model/Package.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Package {
+	private int packageId;
+	private String title;
+	private String location;
+	private double price;
+	private int availableSeats;
+	private String startDate;
+	private String endDate;
+	int PackageId() {
+		return -1;
+		
+	}
+	
+}
