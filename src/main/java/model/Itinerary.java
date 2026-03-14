297bb3d0a48800b8b3cb44b8ffc2c5bc8a786395
Akksheetha <akkshee@gmail.com>
2026-03-14 16:47:11 +0530

    Added attributes

--- src/main/java/model/Itinerary.java
+++ src/main/java/model/Itinerary.java
@@ -1,5 +1,38 @@
 package model;
-
+import java.util.*;
 public class Itinerary {
-
+	private int itineraryId;
+	private int packageId;
+	private int userId;
+	private List<ItineraryItem> items;
+    public Itinerary(int itineraryId, int packageId, int userId, List<ItineraryItem> items) {
+        this.itineraryId = itineraryId;
+        this.packageId = packageId;
+        this.userId = userId;
+        this.items = items;
+    }
+    public int getItineraryId() {
+        return itineraryId;
+    }
+    public void setItineraryId(int itineraryId) {
+        this.itineraryId = itineraryId;
+    }
+    public int getPackageId() {
+        return packageId;
+    }
+    public void setPackageId(int packageId) {
+        this.packageId = packageId;
+    }
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public List<ItineraryItem> getItems() {
+        return items;
+    }
+    public void setItems(List<ItineraryItem> items) {
+        this.items = items;
+    }
 }
--- src/main/java/model/ItineraryItem.java
+++ src/main/java/model/ItineraryItem.java
@@ -1,5 +1,38 @@
 package model;
 
 public class ItineraryItem {
-
+	private int itemId;
+    private int dayNumber;
+    private String activity;
+    private String location;
+    public ItineraryItem(int itemId, int dayNumber, String activity, String location) {
+        this.itemId = itemId;
+        this.dayNumber = dayNumber;
+        this.activity = activity;
+        this.location = location;
+    }
+    public int getItemId() {
+        return itemId;
+    }
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+    public int getDayNumber() {
+        return dayNumber;
+    }
+    public void setDayNumber(int dayNumber) {
+        this.dayNumber = dayNumber;
+    }
+    public String getActivity() {
+        return activity;
+    }
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
