f332fc367c1da79b86a92fed6edd0be98be87423
Harini R G <harinirg26@gmail.com>
2026-03-14 21:08:18 +0530

    The Booking Class is added.

--- src/main/java/model/Booking.java
+++ src/main/java/model/Booking.java
@@ -2,4 +2,54 @@ package model;
 
 public class Booking {
 
+    private int bookingId;
+    private String bookingDate;
+    private int travelers;
+    private double totalAmount;
+    private String status;
+    private int customerId;
+    private int packageId;
+
+    public Booking(int bookingId, String bookingDate, int travelers, double totalAmount,
+                   String status, int customerId, int packageId) {
+        this.bookingId = bookingId;
+        this.bookingDate = bookingDate;
+        this.travelers = travelers;
+        this.totalAmount = totalAmount;
+        this.status = status;
+        this.customerId = customerId;
+        this.packageId = packageId;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public String getBookingDate() {
+        return bookingDate;
+    }
+
+    public int getTravelers() {
+        return travelers;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getPackageId() {
+        return packageId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
