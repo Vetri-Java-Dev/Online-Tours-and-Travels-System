7446912be3969860ddec932ad548ad46ebd08987
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-03-14 12:28:13 +0530

    User management module's Models are created

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+/*preferences : String
+
+contactDetails : String
+
+bookingHistory : List<int>*/
+public class Customer extends User{
+	
+	private String preference;
+	private String contactDetail;
+	private List<Integer> bookingHistory;
+	
+	public Customer(int userId, String name, String email, String password, String phone, String role,
+			String preference, String contactDetail, List<Integer> bookingHistory) {
+		super(userId, name, email, password, phone, role);
+		this.preference = preference;
+		this.contactDetail = contactDetail;
+		this.bookingHistory = bookingHistory;
+	}
+	
+	
+	
+
+}
