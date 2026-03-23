bc5b04453f0ce3eaab080f885886b4e2864f294e
Akksheetha <akkshee@gmail.com>
2026-03-23 09:48:35 +0530

    Modified features

--- src/main/java/comparator/DurationComparator.java
+++ src/main/java/comparator/DurationComparator.java
@@ -5,7 +5,6 @@ import model.TourPackage;
 
 public class DurationComparator implements Comparator<TourPackage> {
 
-    @Override
     public int compare(TourPackage p1, TourPackage p2) {
         return p1.getDuration() - p2.getDuration();
     }
