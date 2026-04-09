f944a5519baa40e01dea3662133d4a7afa369a4b
Akksheetha <akkshee@gmail.com>
2026-04-09 12:30:33 +0530

    Modified Itinerary Package

--- src/main/java/model/Itinerary.java
+++ src/main/java/model/Itinerary.java
@@ -1,41 +1,27 @@
 package model;
-import java.util.*;
+import java.util.List;
+
 public class Itinerary {
-	
-	private int itineraryId;
-	private int packageId;
-	private int userId;
-	private List<ItineraryItem> items;
-	
-    public Itinerary(int itineraryId, int packageId, int userId, List<ItineraryItem> items) {
+
+    private int itineraryId;
+    private int packageId;
+    private List<ItineraryItem> items;
+
+    public Itinerary(int itineraryId, int packageId, List<ItineraryItem> items) {
         this.itineraryId = itineraryId;
         this.packageId = packageId;
-        this.userId = userId;
         this.items = items;
     }
+
     public int getItineraryId() {
         return itineraryId;
     }
-    public void setItineraryId(int itineraryId) {
-        this.itineraryId = itineraryId;
-    }
+
     public int getPackageId() {
         return packageId;
     }
-    public void setPackageId(int packageId) {
-        this.packageId = packageId;
-    }
-    public int getUserId() {
-        return userId;
-    }
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
+
     public List<ItineraryItem> getItems() {
         return items;
     }
-    public void setItems(List<ItineraryItem> items) {
-        this.items = items;
-    }
 }
-
