3b0aea4748a3e705385acffaed567e134d042f54
Harini R G <harinirg26@gmail.com>
2026-04-19 02:00:45 +0530

    The package controller is modified

--- src/main/java/controller/PackageController.java
+++ src/main/java/controller/PackageController.java
@@ -128,7 +128,7 @@ public class PackageController {
     }
 
     // ================= CUSTOMER: SEARCH PACKAGE =================
-    public void customerSearchPackage() {
+    public void customerSearchPackage(int customerId) {
     	System.out.println(ColorText.warning("\n╔══════════════════════════════════════╗"));
         System.out.println(ColorText.warning("║") + ColorText.bold("           SEARCH PACKAGE             ") + ColorText.warning("║"));
         System.out.println(ColorText.warning("╠══════════════════════════════════════╣"));
@@ -171,5 +171,14 @@ public class PackageController {
 
         for (TourPackage t : list)
             System.out.println("  ID: " + t.getPackageId() + " - " + ColorText.cyan(t.getDestination()) + " - Rs." + t.getPrice());
+
+        System.out.print("\nDo you want to book any package? (yes/no): ");
+        String choice = sc.nextLine();
+
+        if (choice.equalsIgnoreCase("yes")) {
+            new BookingController().createBooking(customerId);
+        } else {
+            System.out.println(ColorText.yellow("  Redirecting to Dashboard..."));
+        }
     }
 }
